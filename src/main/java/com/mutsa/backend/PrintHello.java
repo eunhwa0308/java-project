5b4aaf9921dd2801cb5238358e386120621270c2
eunhwa0308 <eunhwa0308@naver.com>
2023-04-18 20:26:09 +0900

    first commit

---
+++ src/main/java/com/mutsa/backend/PrintHello.java
@@ -0,0 +1,7 @@
+package com.mutsa.backend;
+
+public class PrintHello {
+    public void print(){
+        System.out.println(1+1);
+    }
+}
---
+++ src/main/java/com/mutsa/backend/PrintHelloTest.java
@@ -0,0 +1,10 @@
+package com.mutsa.backend;
+
+public class PrintHelloTest {
+    public static void main(String[] args) {
+        PrintHello printHello = new PrintHello();
+        printHello.print();
+        PrintBye printBye = new PrintBye();
+        printBye.print();
+    }
+}
